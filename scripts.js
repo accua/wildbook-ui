86a316cf690a705d294eb8c239544ae51b3809d2
Ezra Sandzer-Bell <equilibriumarts@gmail.com>
2017-04-12 14:22:33 -0700

    add jquery and html content for the third well

--- scripts.js
+++ scripts.js
@@ -23,6 +23,20 @@ $( document ).ready(function() {
         event.preventDefault(); $(this).parent('div').remove();
     });
 
+    // append additional forms to sample protocol
+
+        var wrapper = $(".sampleProtocol"); //Fields wrapper
+        var z = 1;
+        $(".addSampleProtocol").click(function(event){ //on add input button click
+            event.preventDefault();
+            $(wrapper).append('<div><input type="text" name="protocol' + z + '[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+            z++;
+        });
+
+        $(wrapper).on("click",".remove_field", function(event){ //user click on remove text
+            event.preventDefault(); $(this).parent('div').remove();
+        });
+
 
 // Add "hide/show form function"
 
@@ -39,11 +53,11 @@ $( document ).ready(function() {
     var y = 1;
     $(".addMeasureField").click(function(event){ //on add input button click
         event.preventDefault();
-        $(measureWrapper).append('<div><label for="measureName' + y + '[]">Measurement Name:</label><br><input type="text" name="measureName' + y + '[]"/><a href="#" class="remove_field">Remove</a><br><label for="measureType' + y + '[]">Measurement Type (units):</label><br><input type="text" name="measureType' + y + '[]"/><br></div>'); //add input box
+        $(measureWrapper).append('<div class="row"><div class="col-md-5"><label for="measureName' + y + '[]">Measurement Name:</label><br><input type="text" name="measureName' + y + '[]"/></div><div class="col-md-5"><label for="measureType' + y + '[]">Measurement Type (units):</label><br><input type="text" name="measureType' + y + '[]"/><br></div><div class="col-md-2"><a href="#" class="remove_field">Remove</a></div></div>'); //add input box
         y++;
     });
 
     $(measureWrapper).on("click",".remove_field", function(event){ //user click on remove text
-        event.preventDefault(); $(this).parent('div').remove();
+        event.preventDefault(); $(this).parent('div').parent('div').remove();
     });
     });
