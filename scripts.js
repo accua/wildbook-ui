1bdbbf93b9cfe001bace92f8a054c1979c8d884b
Ezra Sandzer-Bell <equilibriumarts@gmail.com>
2017-04-13 11:01:10 -0700

    update css styling for wells, cancelIcon formatting

--- scripts.js
+++ scripts.js
@@ -11,16 +11,15 @@ $(document).ready(function() {
 
 // append additional forms to submission emails
 
-    var wrapper = $(".input_fields_wrap"); //Fields wrapper
     var x = 1;
     $(".addStudySite").click(function(event){ //on add input button click
         event.preventDefault();
-        $(wrapper).append('<div><input type="text" name="mytext' + x + '[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+        $(".input_fields_wrap").append('<div class="row"><div class="col-md-10"><input class="form-control" type="text" name="studySite' + x + '[]"/></div><div class="col-md-1 cancelIconColumn2"><a href="#" class="remove_field"><img src="cancelIcon.png" class="cancelIcon"></a></div><br><br><br></div>'); //add input box
         x++;
     });
 
-    $(wrapper).on("click",".remove_field", function(event){ //user click on remove text
-        event.preventDefault(); $(this).parent('div').remove();
+    $(".input_fields_wrap").on("click",".remove_field", function(event){ //user click on remove text
+        event.preventDefault(); $(this).parent('div').parent().remove();
     });
 
     // append additional forms to sample protocol
@@ -29,12 +28,12 @@ $(document).ready(function() {
         var z = 1;
         $(".addSampleProtocol").click(function(event){ //on add input button click
             event.preventDefault();
-            $(wrapper).append('<div><input type="text" name="protocol' + z + '[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+            $(wrapper).append('<div class="row"><div class="col-md-10"><input class="form-control" type="text" name="protocol' + z + '[]"/></div><div class="col-md-1 cancelIconColumn2"><a href="#" class="remove_field"><img src="cancelIcon.png" class="cancelIcon"></a></div><br><br><br></div>'); //add input box
             z++;
         });
 
         $(wrapper).on("click",".remove_field", function(event){ //user click on remove text
-            event.preventDefault(); $(this).parent('div').remove();
+            event.preventDefault(); $(this).parent('div').parent().remove();
         });
 
 
@@ -53,11 +52,38 @@ $(document).ready(function() {
     var y = 1;
     $(".addMeasureField").click(function(event){ //on add input button click
         event.preventDefault();
-        $(measureWrapper).append('<div class="row"><div class="col-md-5"><label for="measureName' + y + '[]">Measurement Name:</label><br><input type="text" name="measureName' + y + '[]"/></div><div class="col-md-5"><label for="measureType' + y + '[]">Measurement Type (units):</label><br><input type="text" name="measureType' + y + '[]"/><br></div><div class="col-md-2"><a href="#" class="remove_field">Remove</a></div></div>'); //add input box
+        $(measureWrapper).append('<div class="row"><div class="col-md-5"><label for="measureName' + y + '[]">Measurement Name:</label><br><input class="form-control" type="text" name="measureName' + y + '[]"/></div><div class="col-md-5"><label for="measureType' + y + '[]">Measurement Type (units):</label><br><input class="form-control" type="text" name="measureType' + y + '[]"/>&nbsp&nbsp</div><div class="col-md-1 cancelIconColumn"><a href="#" class="remove_field"><img src="cancelIcon.png" class="cancelIcon"></a></div><br><br></div><br>'); //add input box
         y++;
     });
 
     $(measureWrapper).on("click",".remove_field", function(event){ //user click on remove text
         event.preventDefault(); $(this).parent('div').parent('div').remove();
     });
+
+    $(".cat1").click(function(){
+      $("#well1").show();
+      $("#well2").hide();
+      $("#well3").hide();
+    });
+    $(".cat2").click(function(){
+      $("#well1").hide();
+      $("#well2").show();
+      $("#well3").hide();
+    });
+    $(".cat3").click(function(){
+      $("#well1").hide();
+      $("#well2").hide();
+      $("#well3").show();
+    });
+    $(".cat4").click(function(){
+      $("#well1").hide();
+      $("#well2").hide();
+      $("#well3").hide();
+    });
+    $(".cat5").click(function(){
+      $("#well1").hide();
+      $("#well2").hide();
+      $("#well3").hide();
+    });
+
     });
